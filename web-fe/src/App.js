b9deedd39bc84671a0be5bacc942d74b5f24ac24
oreozzang <93peace@naver.com>
2022-08-16 21:08:06 +0900

    feat: 라우팅완료

--- web-fe/src/App.js
+++ web-fe/src/App.js
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import './App.css';
 import Main from './components/Main';
 import Vote from './components/Voting/Vote';
+import Polling from './components/Voting/Polling';
+import Executive from './components/Voting/Executive';
+import Howto from './components/Voting/Howto';
+
 import { Route,Link } from 'react-router-dom';
 
 
@@ -11,6 +15,9 @@ function App() {
   return (
     <div>
       <Route path="/voting" component={Vote}/>
+        <Route path="/voting/polling" component={Polling}></Route>
+        <Route path="/voting/executive" component={Executive}></Route>
+        <Route path="/voting/howto" component={Howto}></Route>
       <Route exact path="/" component={Main}></Route>
       {/* Vote Page, 아직 메인에 들어갈지 해더에 들어갈지 안정했기 때문에 일단 App에 넣어두었음 */}
       <Link to="/voting">Vote page</Link>
--- web-fe/src/components/Voting/Header/Header.js
+++ web-fe/src/components/Voting/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Header(){
     return(
         <header className={styles.header}>
             <nav>
-                <div>MAIN</div>
-                <div>Polling</div>
-                <div>Executive</div>
-                <div>How to</div>
+                <div><Link to='/voting'>MAIN</Link></div>
+                <div><Link to='/voting/polling'>Polling</Link></div>
+                <div><Link to='/voting/executive'>Executive</Link></div>
+                <div><Link to='/voting/howto'>How to</Link></div>
             </nav>
             <div>
                 <div>메인넷</div>
