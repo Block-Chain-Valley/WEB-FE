dfb953fdc0d94f44b47648f53216311301fee0dd
oreozzang <93peace@naver.com>
2022-08-17 15:14:07 +0900

    feat: header

--- web-fe/src/components/Voting/Header/Header.js
+++ web-fe/src/components/Voting/Header/Header.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import Walletbutton from '../Walletbutton/Walletbutton';
+import dataWalletbutton from'./dataWalletbutton.json'
 
 export default function Header(){
+    
+    let [toggle,setToggle] = useState(false);
+
     return(
         <header className={styles.header}>
-            <nav>
-                <div><Link to='/voting'>MAIN</Link></div>
-                <div><Link to='/voting/polling'>Polling</Link></div>
-                <div><Link to='/voting/executive'>Executive</Link></div>
-                <div><Link to='/voting/howto'>How to</Link></div>
+            <nav className={styles.nav}>
+                <Link className={styles.Link} to='/voting'><img className={styles.headerlogo} src='img/BV_Logo.svg'/></Link>
+                {   
+                    toggle
+                    ?<ul className={styles.ul}>
+                        <li className={styles.li}><Link className={styles.Link} to='/voting/polling'>Polling</Link></li>
+                        <li className={styles.li}><Link className={styles.Link} to='/voting/executive'>Executive</Link></li>
+                        <li className={styles.li}><Link className={styles.Link} to='/voting/howto'>How to</Link></li>
+                    </ul>
+                    :null
+                }
+                
             </nav>
-            <div>
-                <div>메인넷</div>
-                <div>connect wallet</div>
-                <div>etc</div>
-            </div>
+            {
+                toggle
+                ?
+                    <div className={styles.walletbuttoncontainer}>
+                    {
+                        dataWalletbutton.map((item,index)=>{
+                            return(<Walletbutton imgURL={item.imgURL} buttonTitle={item.buttonTitle} className={styles.walletbutton}></Walletbutton>)
+                        })
+                    }
+                    </div>
+                :
+                    null
+            }
+            
+            
+            <a className={styles.toggleBtn} href='#' onClick={()=>{setToggle(!toggle);}}>
+                <img className={styles.toggleImg} src='img/hamburger.png'></img>
+            </a>
         </header>
     )
 }
--- web-fe/src/components/Voting/Walletbutton/Walletbutton.js
+++ web-fe/src/components/Voting/Walletbutton/Walletbutton.js
@@ -1,11 +1,20 @@
-import styles from "./Walletbutton.modul.css"
+import styles from "./Walletbutton.module.css"
 
 
 export default function Walletbutton(Props){
     return(
-        <button>
-            <img></img>
-            {Props.title}
+        <button className={styles.button}>
+            {
+                Props.imgURL==""
+                ? null
+                :<img alt="" src={Props.imgURL} ></img>
+            }
+            {
+                Props.buttonTitle==""
+                ? null
+                : <div>{Props.buttonTitle}</div>
+
+            }
         </button>
     )
 }
