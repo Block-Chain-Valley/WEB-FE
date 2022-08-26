0fa342ea8644baa402f40cc5815fbd4b0921e7eb
oreozzang <93peace@naver.com>
2022-08-26 17:59:31 +0900

    feat:walletfaviconadd

--- web-fe/src/components/Voting/ConnectWalletbutton/ConnectWalletbutton.js
+++ web-fe/src/components/Voting/ConnectWalletbutton/ConnectWalletbutton.js
@@ -3,15 +3,15 @@ import Modal from "../Modal/Modal"
 import styles from "./ConnectWalletbutton.module.css"
 
 
-export default function ConnectWalletbutton(Props){
+export default function ConnectWalletbutton(){
     const [modalToggle,setModalToggle] = useState(false);
 
     return(
-        <div className={styles.button} onClick={()=>{setModalToggle(true)}}>
+        <div className={styles.button} onClick={()=>{setModalToggle(true); console.log(setModalToggle,modalToggle);}}>
             Connect wallet
             {
                 modalToggle
-                ?<Modal setModalToggle={setModalToggle}></Modal>
+                ?<Modal setModalToggle={setModalToggle} modalToggle={modalToggle}></Modal>
                 :null
             }
         </div>
--- web-fe/src/components/Voting/Modal/Modal.js
+++ web-fe/src/components/Voting/Modal/Modal.js
@@ -1,17 +1,22 @@
 import styles from "./Modal.module.css"
 
-export default function Modal(){
+export default function Modal({modalToggle,setModalToggle}){
     return(
         <div className={styles.modalbox}>
             <div className={styles.modal}>
-                <div className={styles.modaltop}>
+                <div className={styles.modaltop} onClick={()=>{setModalToggle(false); console.log(modalToggle,setModalToggle)}}>
                     <span>
                         Select Wallet  
                     </span>
-                    <img src={process.env.PUBLIC_URL+'/img/x-button.svg'}></img>
+                    <img src={process.env.PUBLIC_URL+'/img/x-button.svg'} ></img>
                 </div>
                 <div className={styles.walletlist}>
-                    <span>Kilp</span>
+                    <img src={process.env.PUBLIC_URL+"/img/클립파비콘.png"}></img>
+                    <span>  Kilp</span>
+                </div>
+                <div className={styles.walletlist}>
+                    <img src={process.env.PUBLIC_URL+"/img/카이카스파비콘.jpeg"}></img>
+                    <span>  Kaikas</span>
                 </div>
             </div>
         </div>
